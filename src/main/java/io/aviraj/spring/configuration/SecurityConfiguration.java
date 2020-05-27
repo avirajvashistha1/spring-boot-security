9044dfc5077d37e23c872efd106939272f4df159
Aviraj <Aviraj@DESKTOP-TVPIGGE>
2020-05-27 09:21:11 +0530

    first draft of security app.

---
+++ src/main/java/io/aviraj/spring/configuration/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package io.aviraj.spring.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.inMemoryAuthentication().withUser("generaluser").password("password").roles("ANY_USER").and()
+				.withUser("user").password("password").roles("USER").and().withUser("admin").password("password")
+				.roles("ADMIN");
+
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/admin").hasRole("ADMIN")
+				.antMatchers("/user").hasAnyRole("USER", "ADMIN").and().formLogin();
+
+	}
+
+}
